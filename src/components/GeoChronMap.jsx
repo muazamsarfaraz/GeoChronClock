e7aa9d27e755182e7dd2937deacef7c9ea1225e7
muazamsarfaraz <muazam@asomas.ai>
2025-04-09 23:31:44 +0100

    Fix JSX syntax error in TileLayer attribution

--- src/components/GeoChronMap.jsx
+++ src/components/GeoChronMap.jsx
@@ -91,7 +91,7 @@ const GeoChronMap = () => {
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"
+          attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
         />
 
         {/* Day/Night Overlay */}
